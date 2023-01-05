e700b0756f4c69ec6d2e7a93075ef6ef31fb72cd
kayaFatih99 <kayafatih051@gmail.com>
2023-01-05 14:17:22 +0300

    react-odev-2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,149 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  console.log("UPDATE");
+  const [todos, setTodos] = useState([
+    {
+      label: "Learn JavaScript",
+      isChecked: true,
+    },
+    {
+      label: "Learn React",
+      isChecked: false,
+    },
+    {
+      label: "Have a life!",
+      isChecked: false,
+    },
+  ]);
+
+  const [statusFilter, setStatusFilter] = useState("All");
+
+  const changeCheckBox = (todo) => {
+    const myTodo = todos.find((td) => td == todo);
+    myTodo.isChecked = !todo.isChecked;
+    setTodos([...todos]);
+  };
+
+  const clearCompletedTodo = () => {
+    const activeTodos = todos.filter((todo) => todo.isChecked === false);
+    setTodos(activeTodos);
+  };
+
+  const removeTodo = (todo) => {
+    const myTodoIndex = todos.indexOf(todo);
+    const newTodos = [...todos];
+
+    newTodos.splice(myTodoIndex, 1);
+    setTodos(newTodos);
+  };
+
+  const filterFunction = (statusFilter) => {
+    if (statusFilter === "All") {
+      return [...todos];
+    } else if (statusFilter === "Active") {
+      return todos.filter((todo) => todo.isChecked === false);
+    }
+    return todos.filter((todo) => todo.isChecked === true);
+  };
+
+  const filteredTodos = filterFunction(statusFilter);
+  const activeTodosLength = todos.filter((t) => !t.isChecked).length;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <form>
+            <input
+              className="new-todo"
+              placeholder="What needs to be done?"
+              autoFocus
+            />
+          </form>
+        </header>
+
+        <section className="main">
+          <input className="toggle-all" type="checkbox" />
+          <label htmlFor="toggle-all">Mark all as complete</label>
+
+          <ul className="todo-list">
+            {filteredTodos.map((todo, i) => {
+              return (
+                <li key={i} className={todo.isChecked ? "completed" : ""}>
+                  <div className="view">
+                    <input
+                      className="toggle"
+                      type="checkbox"
+                      checked={todo.isChecked}
+                      onChange={() => {
+                        changeCheckBox(todo);
+                      }}
+                    />
+                    <label>{todo.label}</label>
+                    <button
+                      className="destroy"
+                      onClick={() => removeTodo(todo)}
+                    ></button>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </section>
+        <footer className="footer">
+          <span className="todo-count">
+            <strong>{activeTodosLength}</strong>
+            items left
+          </span>
+
+          <ul className="filters">
+            <li>
+              <a
+                href="#/"
+                className={statusFilter === "All" ? "selected" : ""}
+                onClick={() => setStatusFilter("All")}
+              >
+                All
+              </a>
+            </li>
+            <li>
+              <a
+                href="#/"
+                className={statusFilter === "Active" ? "selected" : ""}
+                onClick={() => setStatusFilter("Active")}
+              >
+                Active
+              </a>
+            </li>
+            <li>
+              <a
+                href="#/"
+                className={statusFilter === "Completed" ? "selected" : ""}
+                onClick={() => setStatusFilter("Completed")}
+              >
+                Completed
+              </a>
+            </li>
+          </ul>
+
+          <button className="clear-completed" onClick={clearCompletedTodo}>
+            Clear completed
+          </button>
+        </footer>
+      </section>
+
+      <footer className="info">
+        <p>Click to edit a todo</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Created by <a href="https://d12n.me/">Dmitry Sharabin</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          Part of <a href="http://todomvc.com">TodoMVC</a>
+        </p>
+      </footer>
     </div>
   );
 }
